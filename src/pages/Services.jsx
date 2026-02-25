461c4727e591265c167e28dfa9e9e6ace71cb3d1
kameraizo <kameraizo69@gmail.com>
2026-02-25 17:40:07 +0100

    props sur les cartes pages services

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,3 +1,19 @@
+    const CardServices = (props) => {
+      return (
+        <div className="col-md-4">
+          <div className="card h-100 shadow-sm text-center">
+            <div className="card-body">
+              <div className="mb-3">
+                <i className={`bi ${props.icon} fs-1 text-primary`}></i>
+                <h2 className="card-title">{props.title}</h2>
+                <p className="card-text">{props.text}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+
 export default function Services() {
   return (
   <div style={{paddingTop: "56px"}}>
@@ -19,51 +35,33 @@ export default function Services() {
       {/*cards*/}
 <div className="container" style={{marginBottom: "80px", marginTop: "20px"}}>   
   <div className="row">
-    <div className="col-md-4"> 
-      <div className="card h-100 shadow-sm text-center">
-        <div className="card-body">
-              <div className="mb-3">
-                <i className="bi bi-brush fs-1 text-primary"></i>
-              </div>
-                <h2 className="card-title">UX Design</h2>
-                <p className="card-text">
-                  Sin autem ad adulescentiam perduxissent, dirimi tamen interdum contentione vel uxoriae condicionis vel
+
+    {/*card1*/}
+        <CardServices
+        icon="bi-brush"
+        title="UX Design"
+        text="Sin autem ad adulescentiam perduxissent, dirimi tamen interdum contentione vel uxoriae condicionis vel
                commodi alicuius, quod idem adipisci uterque non posset. Quod si qui longius in amicitia provecti essent,
-                tamen saepe labefactari, si in honoris contentionem incidissent; pestem enim nullam maiorem esse amicitiis.
-                </p>
-        </div>
-      </div>
-    </div>
-        <div className=" col-md-4"> 
-          <div className="card h-100 shadow-sm text-center">
-            <div className="card-body">
-              <div className="mb-3">
-                <i className="bi bi-code-slash fs-1 text-primary"></i>
-              </div>
-                <h2 className="card-title">Developpement web</h2>
-                <p className="card-text">
-                  Sin autem ad adulescentiam perduxissent, dirimi tamen interdum contentione vel uxoriae condicionis vel
+                tamen saepe labefactari, si in honoris contentionem incidissent; pestem enim nullam maiorem esse amicitiis."
+        />
+
+        {/*card2*/}
+        <CardServices
+        icon="bi-code-slash"
+        title="Developpement web"
+        text="Sin autem ad adulescentiam perduxissent, dirimi tamen interdum contentione vel uxoriae condicionis vel
                commodi alicuius, quod idem adipisci uterque non posset. Quod si qui longius in amicitia provecti essent,
-                tamen saepe labefactari, si in honoris contentionem incidissent; pestem enim nullam maiorem esse amicitiis.
-                </p>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4"> 
-          <div className="card h-100 shadow-sm text-center">
-            <div className="card-body">
-              <div className="mb-3">
-                <i className="bi bi-search fs-1 text-primary"></i>
-              </div>
-                <h2 className="card-title">Référencement</h2>
-                <p className="card-text">
-                  Sin autem ad adulescentiam perduxissent, dirimi tamen interdum contentione vel uxoriae condicionis vel
+                tamen saepe labefactari, si in honoris contentionem incidissent; pestem enim nullam maiorem esse amicitiis."
+        />
+
+        {/*card3*/}
+        <CardServices
+        icon="bi-search"
+        title="Référencement"
+        text="Sin autem ad adulescentiam perduxissent, dirimi tamen interdum contentione vel uxoriae condicionis vel
                commodi alicuius, quod idem adipisci uterque non posset. Quod si qui longius in amicitia provecti essent,
-                tamen saepe labefactari, si in honoris contentionem incidissent; pestem enim nullam maiorem esse amicitiis.
-                </p>
-            </div>
-          </div>
-        </div>
+                tamen saepe labefactari, si in honoris contentionem incidissent; pestem enim nullam maiorem esse amicitiis."
+        />
   </div>
 </div> 
   </div>
